646ed3a7d42a721b01ae57d031dc787bdcb0795d
theGamer168 <53354557+theGamer168@users.noreply.github.com>
2020-09-16 21:27:13 +0200

    Delete Tower.py

--- Tower.py
+++
@@ -1,8 +0,0 @@
-def tower_builder(n_floors):
-    width = (2*n_floors)-1
-    result= []
-    for i in range(1,2*n_floors,2):
-        stars = '*'*i
-        tower = stars.center(width)
-        result.append(tower)
-    return result
